bea3097cd131bbeda4162ef0910a7bef39ada1b5
Mayank <karmakar.d.mayank@gmail.com>
2025-10-08 00:08:40 +0530

    finalized AlbumList css

--- src/components/AlbumList/AlbumList.js
+++ src/components/AlbumList/AlbumList.js
@@ -1,42 +1,20 @@
 import React from "react";
 import "./AlbumList.css";
-import { BiSolidPhotoAlbum } from "react-icons/bi";
+import { BiPhotoAlbum } from "react-icons/bi";
 
 const AlbumList = () => {
   return (
     <div className="album-list">
       <div className="album-list-head">
-        <h2>Your Album</h2>
-        <button>Add album</button>
+        <h2>Your Albums</h2>
+        <button className="addBtn">Add album</button>
       </div>
       <div className="album-grids">
         <div className="album-card">
-          <BiSolidPhotoAlbum />
-          <h3>Album 1</h3>
-        </div>
-        <div className="album-card">
-          <BiSolidPhotoAlbum />
-          <h3>Album 2</h3>
-        </div>
-        <div className="album-card">
-          <BiSolidPhotoAlbum />
-          <h3>Album 2</h3>
-        </div>
-        <div className="album-card">
-          <BiSolidPhotoAlbum />
-          <h3>Album 2</h3>
-        </div>
-        <div className="album-card">
-          <BiSolidPhotoAlbum />
-          <h3>Album 2</h3>
-        </div>
-        <div className="album-card">
-          <BiSolidPhotoAlbum />
-          <h3>Album 2</h3>
-        </div>
-        <div className="album-card">
-          <BiSolidPhotoAlbum />
-          <h3>Album 2</h3>
+          <div className="album-icon-container">
+            <BiPhotoAlbum className="album-icon" />
+          </div>
+          <h3 className="album-title">Album 1</h3>
         </div>
       </div>
     </div>
